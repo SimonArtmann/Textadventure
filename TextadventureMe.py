439481e6e91a07e09d38197657e968b59cf055ae
Simon Artmann <simonart0605@gmail.com>
2021-10-10 16:06:55 +0200

    Test

--- TextadventureMe.py
+++ TextadventureMe.py
@@ -13,10 +13,10 @@ class HealthPotion(Item):
 
 class Character:
     def __init__(self, hp, ad, name, xp):
-        self.hp = hp
-        self.ad = ad
-        self.name = name
-        self.xp = xp
+        self.hp =  hp
+        self.ad =  ad
+        self.name =  name
+        self.xp =  xp
 
     def get_hit(self, ad):
         self.hp = self.hp - ad
